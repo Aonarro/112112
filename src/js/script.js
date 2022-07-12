481e52018b4964944a5874ad4baae16d2ded96dd
Nikolay <nikolya.tyshchenko04@gmail.com>
2022-07-13 00:32:45 +0300

    adding animations

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
   $(".carousel__inner").slick({
     speed: 1200,
-    // adaptiveHeight: true,
+    adaptiveHeight: true,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
     nextArrow:
@@ -12,11 +12,11 @@ $(document).ready(function () {
         settings: {
           dots: true,
           arrows: false,
-          swipeToSlide: true,
         },
       },
     ],
   });
+
   $("ul.catalog__tabs").on(
     "click",
     "li:not(.catalog__tab_active)",
@@ -47,17 +47,87 @@ $(document).ready(function () {
 
   toggleSlide(".catalog-item__link");
   toggleSlide(".catalog-item__back");
-  //modal
+
+  // Modal
+
   $("[data-modal=consultation]").on("click", function () {
     $(".overlay, #consultation").fadeIn("slow");
   });
   $(".modal__close").on("click", function () {
     $(".overlay, #consultation, #thanks, #order").fadeOut("slow");
   });
+
   $(".button_mini").each(function (i) {
     $(this).on("click", function () {
       $("#order .modal__descr").text($(".catalog-item__subtitle").eq(i).text());
       $(".overlay, #order").fadeIn("slow");
     });
   });
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true,
+        },
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символа!"),
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты",
+        },
+      },
+    });
+  }
+
+  validateForms("#consultation-form");
+  validateForms("#consultation form");
+  validateForms("#order form");
+
+  $("input[name=phone]").mask("+380 (99) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  // Smooth scroll and pageup
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1600) {
+      $(".pageup").fadeIn();
+    } else {
+      $(".pageup").fadeOut();
+    }
+  });
+
+  $("a[href=#up]").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
+
+  new WOW().init();
 });
