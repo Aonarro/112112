561782711fea8197ddc6da94eb0675719cc91668
Nikolay <nikolya.tyshchenko04@gmail.com>
2022-07-13 16:44:37 +0300

    adding animate.css

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
   $(".carousel__inner").slick({
     speed: 1200,
-    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 1200,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
     nextArrow:
